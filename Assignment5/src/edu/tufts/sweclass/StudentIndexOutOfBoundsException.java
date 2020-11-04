c5a5b1593133c3e052419083e044e75c55d491f2
jtreff01 <71662661+jtreff01@users.noreply.github.com>
2020-11-04 18:58:14 -0500

    Update StudentIndexOutOfBoundsException.java

--- Assignment5/src/edu/tufts/sweclass/StudentIndexOutOfBoundsException.java
+++ Assignment5/src/edu/tufts/sweclass/StudentIndexOutOfBoundsException.java
@@ -3,8 +3,8 @@ package edu.tufts.sweclass;
 public class StudentIndexOutOfBoundsException extends RuntimeException {
 	private static final long serialVersionUID = 8226094121089000000L;
 
-	public StudentIndexOutOfBoundsException(String message) {
-		super(message);
+	public StudentIndexOutOfBoundsException(String Message) {
+		super(Message);
 	}
 
 	public StudentIndexOutOfBoundsException() {
